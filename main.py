6de04f428d04f38ebe90a1d753707c8dd1674fa3
Ilya <karpov.ilja@gmail.com>
2024-02-28 10:41:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# Dokumentācija
+# Mainīgie - https://www.w3schools.com/python/python_variables.asp
+# Operācijas ar mainīgiem - https://www.w3schools.com/python/python_operators.asp
+# Mainīgo drukāšana - https://www.w3schools.com/python/python_variables_output.asp
+# Nosacījumi, zarošana, if ... else - https://www.w3schools.com/python/python_conditions.asp
+# For cikls - https://www.w3schools.com/python/python_for_loops.asp
+# Nejauša skaitļa generēšana - https://www.w3schools.com/python/ref_random_randint.asp
+# Github Fork (repozitorija kopija) - https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo
+# Saraksti - https://www.w3schools.com/python/python_lists.asp
+# Vārdnīcas - https://www.w3schools.com/python/python_dictionaries.asp
+# Klonēt repozitoriju - hhttps://code.visualstudio.com/docs/sourcecontrol/intro-to-git
 
