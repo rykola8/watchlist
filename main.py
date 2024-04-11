66a524a2523fbdc234635dd1bee5ed75a5d62f93
Your Name <you@example.com>
2024-04-11 17:18:03 +0300

    films

--- main.py
+++ main.py
@@ -15,80 +15,112 @@ films = []
 
 
 #izveidoju bezgalīgu cilpu, lai sniegtu izvēli, līdz tiek izpildīts nosacījums, kas to izbeigs.
-while True: 
-    command = input("\nChoose command: 1. pievienot filmu norādot nosaukumu un reitingu, 2. dzēst filmu no saraksta, 3. atspoguļot top 10 filmas pec reitinga, 4. iztukšot sarakstu, 5. meklēt filmu pēc nosaukuma daļas, 6. atzīmēt filmu kā skatīto, 7. atzīmēt filmu kā neskatīto, 8.atfiltrēt noskatītas/nenoskatītas filmas: ")
-    if command == "1":
+while True:
+     print("1. pievienot filmu norādot nosaukumu un reitingu, atzīmēt filmu kā skatīto/neskatīto ") 
+     print("2. dzēst filmu no saraksta")
+     print("3. atspoguļot top 10 filmas pec reitinga")
+     print("4. iztukšot sarakstu")
+     print("5. meklēt filmu pēc nosaukuma daļas")
+     print("6. atfiltrēt noskatītas/nenoskatītas filmas")
+     print("7. import json")
+     print("8. Exit")
+     command = input("Choose command: ") #lietotājs ievada savu izvēli.
+
+     if command == "1": #ja viņš izvēlējās 1, tiek izpildīts šis kods.
        dictionary_films = {}
        
        name = input('Enter the film name: ')
+       if len(name) < 2: #teikts, ka, ja nosaukumā ir mazāk par divām rakstzīmēm, tiks ģenerēta "kļūda"
+            print("Error")
+            continue
+       if len(name) > 120: #teikts, ka, ja nosaukumā ir vairāk nekā simt divdesmit rakstzīmju, tiks ģenerēta "kļūda"
+            print("Error")
+            continue
        dictionary_films["name"] = name
 
        rating = input('Enter the film rating: ')
+       if not rating.isdigit(): #pārbauda, vai tas ir vesels skaitlis 
+            print("Error")
+            continue
+       if int(rating) < 1 or int(rating) > 10: #pārbauda, vai tas ir mazāks par 1 vai lielāks par 10.
+            print("Error")
+            continue
        dictionary_films["rating"] = int(rating)
+
+       reviewed_yes_or_no = input('Have you seen this movie?: ')
+       dictionary_films["reviewed"] = reviewed_yes_or_no
 
        films.append(dictionary_films)
        print(films) 
-    if command == "2":
-        
+
+     if command == "2": #ja viņš izvēlējās 2, tiek izpildīts šis kods.
         number_delete = int(input("enter number of film: "))
         del films[number_delete]
         print(films)
-        pass
+        pass 
+     
     
-    if command == "3":
-        def my_func(films):
+     if command == "3": #ja viņš izvēlējās 3, tiek izpildīts šis kods.
+          def my_func(films):
             return films["rating"]
-        
-        films.sort(key = my_func, reverse = True)
-        print(films[0:9])
-        
-    if command == "4":
-        films.clear()
-        print(films)
-        pass
-    if command == "5":
-        name_film = input('Enter the film name: ')
-        new_films = []
-        # izvēlos katru vārdnīcu no saraksta 
-        for film in films:
-            if film['name'] == name_film:  #pārbaudu, vai filmas nosaukums vārdnīcā sakrīt ar lietotāja rakstīto nosaukumu
-                new_films.append(x)     #Ja tas atbilst, es to pievienoju new_films
+            
+          films.sort(key = my_func, reverse = True)
+          print(films[0:9])
 
-        print(new_films)
-
-    if command == "6":
-        name_film = input('Enter the film name: ')
-        # izvēlos katru vārdnīcu no saraksta   
-        for film in films:
-            if film['name'] ==  name_film:  #pārbaudu, vai filmas nosaukums vārdnīcā sakrīt ar lietotāja rakstīto nosaukumu
-                film['reviewed'] = "yes"  #ja tas atbilst, es pievienoju ['pārskatīts'] = "jā".
-        print(films)   
+     if command == "4": #ja viņš izvēlējās 4, tiek izpildīts šis kods.
+            films.clear()
+            print(films)
+            pass
     
-    if command == "7":  
-        name_film = input('Enter the film name: ')
-        # izvēlos katru vārdnīcu no saraksta  
-        for film in films:
-            if film['name'] ==  name_film: #pārbaudu, vai filmas nosaukums vārdnīcā sakrīt ar lietotāja rakstīto nosaukumu
-                film['reviewed'] = "no"  #ja tas atbilst, es pievienoju ['pārskatīts'] = "nē".
-        print(films)   
-    if command == "8":
-        reviewed = input("yes or no: ")
-        new_films = []
-        # izvēlos katru vārdnīcu no saraksta  
-        for film in films:
-            if film['reviewed'] == reviewed: #pārbaudīt, vai filma ir skatīta vai nē (atkarībā no lietotāja izvēles)
-                new_films.append(x)
-        print(new_films)
+     if command == "5": #ja viņš izvēlējās 5, tiek izpildīts šis kods.
+            name_film = input('Enter the film name: ')
+            new_films = []
+            # izvēlos katru vārdnīcu no saraksta 
+            for film in films:
+                if film['name'] == name_film:  #pārbaudu, vai filmas nosaukums vārdnīcā sakrīt ar lietotāja rakstīto nosaukumu
+                    new_films.append(film)     #Ja tas atbilst, es to pievienoju new_films
+            print(new_films)
+
+     if command == "6": #ja viņš izvēlējās 6, tiek izpildīts šis kods.
+            reviewed = input("yes or no: ")
+            new_films = []
+            # izvēlos katru vārdnīcu no saraksta  
+            for film in films:
+                if film['reviewed'] == reviewed: #pārbaudīt, vai filma ir skatīta vai nē (atkarībā no lietotāja izvēles)
+                    new_films.append(film)
+            print(new_films) 
+
+    
+     if command == "7": #ja viņš izvēlējās 7, tiek izpildīts šis kods.
+          import json
+          with open('films.json', 'w') as file:
+            json.dump(films, file)
+
+          with open('films.json', 'r') as file:
+            films_data = json.load(file)
+            print(films_data)
+
+     if command == "8": #ja viņš izvēlējās 8, tiek izpildīts šis kods.
+            print("Exiting...")
+            break 
+
+    
+
+
+   
+   
+   
+   
+
     
     
-    if command == "e":
-        print("Exiting...")
-        break
-
-
-
-import json
-f = open('films.json',) 
-data = json.load(f)
+    
+  
+   
+    
+ 
+      
+    
+    
 
 
